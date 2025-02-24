babcc6db2b0ae0b68f5c16740bb94e58b8121e4b
Evan McKenzie <evan.mcken@gmail.com>
2025-02-23 21:16:41 -0800

    solved interpolation to get means of curvature between individuals within species

--- curve_code.R
+++ curve_code.R
@@ -49,51 +49,69 @@ peak.filt.curv <- curvatures %>%
 curvatures.aligned <- curvatures %>% 
   left_join(peak.filt.curv, by = "indvd") %>%
   group_by(indvd) %>%
-  mutate(peak.start.perim = perim_start - perim_subtract) %>%
-  mutate(pos.peak.start = c(peak.start.perim[peak.start.perim >= 1], 
-                            rev(abs(peak.start.perim[peak.start.perim < 1]) + max(peak.start.perim)))) %>%
+  mutate(peak.start.perim = perim_start - perim_subtract) %>% # centers zero on the first peak, axis now has + and -
+  mutate(pos.peak.start = c(peak.start.perim[peak.start.perim >= 0], 
+                            rev(abs(peak.start.perim[peak.start.perim < 0]) + max(peak.start.perim)))) %>% # same as perim_start?
   mutate(curv.shift = c(Curvature[perim_start >= perim_subtract],
-                        Curvature[perim_start < perim_subtract])) %>% # remake curvature column so that it aligns with the new x-axis (formerly negative region needs to be reversed)
+                        Curvature[perim_start < perim_subtract])) %>% # remake curvature column so that it aligns with the new x-axis
+  mutate(norm_pos.peak.start = pos.peak.start / max(pos.peak.start)) %>%
+  #mutate(norm2_pos.peak.start = (pos.peak.start - min(pos.peak.start)) / max(pos.peak.start)) %>%
   #filter(species == "anomalus") %>%
   print()
 
 str(curvatures.aligned)
 
+curvatures.aligned_normalized <- curvatures.aligned %>%
+  group_by(indvd) %>%
+  mutate(norm_pos.peak.start = (pos.peak.start - min(pos.peak.start)) / (max(pos.peak.start) - min(pos.peak.start))) %>%
+  ungroup() %>%
+  print()
+
+# Create a common normalized x-axis
+common_pos_peak_start <- seq(0, 1, length.out = 1000)
+
+# Interpolate curv.shift for each individual
+curvatures.aligned_interpolated <- curvatures.aligned_normalized %>%
+  group_split(indvd) %>%  # Split by individual
+  map_dfr(~ {
+    tibble(
+      indvd = unique(.x$indvd),
+      norm_pos.peak.start = common_pos_peak_start,  # Use the same common x-axis
+      curv.shift = approx(
+        x = .x$norm_pos.peak.start, 
+        y = .x$curv.shift, 
+        xout = common_pos_peak_start, 
+        rule = 2
+      )$y
+    )
+  }) %>%
+  mutate(species = substr(indvd, 1, nchar(indvd)-2)) %>% #drop ndividual number
+  ungroup() %>%
+  group_by(species, norm_pos.peak.start) %>%
+  mutate(mean_curv.shift = mean(curv.shift))
+
+check.row <- curvatures.aligned_interpolated %>%
+  group_by(indvd) %>%
+  summarise(n_norm_pos.peak.start = length(norm_pos.peak.start), n_curv.shift = length(curv.shift))
+
 
 # test some plots to make sure
-ggplot(data = filter(curvatures.aligned, indvd == "trivittatus 7"), 
+ggplot(data = filter(curvatures.aligned_interpolated, indvd == "trivittatus 7"), 
        #aes(x = perim_start, y = Curvature)
        #aes(x = peak.start.perim, y = Curvature)
        #aes(x = pos.peak.start, y = Curvature)
-       aes(x = pos.peak.start, y = curv.shift)
-       
+       aes(x = norm_pos.peak.start, y = curv.shift)
        ) +
   geom_point() #+
 #xlab("perimeter position") #+
 #geom_vline(xintercept = 1298)
 
-
-
-####    find peaks as a separate df    ####  subsequent weird shifting
-# peak.filt.curv <- curvatures %>%
-#   mutate(peaks = find_peaks(Curvature, ignore_threshold = 0.6, span = 3, strict = FALSE)) %>% # column indicating peaks
-#   filter(peaks == "TRUE") %>% # make peaks findable
-#   filter(!(species == "americanus" & Curvature < 0.0056)) %>%
-#   # filter(!(species == "anomalus" & Curvature < 0.03)) %>%
-#   # filter(!(species == "pallidipes" & Curvature < 0.0275)) %>%
-#   # filter(!(species == "edulis" & Curvature < 0.02)) %>%
-#   # filter(!(species == "trivittatus" & Curvature < 0.02)) %>%
-#   print()
-
-# slide the data so it always starts at the first peak (right hand sac tip in images)
-curvatures.aligned <- curvatures %>%
-  group_by(indvd) %>%
-  mutate(peak.start.perim = perim_start - peak.filt.curv$perim_start[1]) %>% # slide the x-axis zero to be at the first peak (now has + and -)
-  mutate(pos.peak.start = c(peak.start.perim[peak.start.perim >= 0],
-                            (abs(peak.start.perim[peak.start.perim < 0])) + max(peak.start.perim))) %>% # remake x-axis to start from zero, and add negative values to the end as positive
-  mutate(curv.shift = c(Curvature[perim_start >= peak.filt.curv$perim_start[1]],
-                        rev(Curvature[perim_start < peak.filt.curv$perim_start[1]]))) %>% # remake curvature column so that it aligns with the new x-axis (formerly negative region needs to be reversed)
-  print()
+ggplot(data = curvatures.aligned_interpolated,
+       aes(x = norm_pos.peak.start, y = mean_curv.shift,
+           group = species,
+           colour = species)
+       ) +
+  geom_point()
 
 
 
@@ -101,7 +119,7 @@ curvatures.aligned <- curvatures %>%
 #####   single indvd wrangle
 
 # figuring out manipulation on a small scale
-test.plot <- read_csv("~/student_documents/UBC/Research/Writing, talks, notes\\edulis Science\\contour stuff/americanus 2.csv") %>%
+test.plot <- read_csv("~/student_documents/UBC/Research/Writing, talks, notes\\edulis Science\\contour stuff/anomalus 8.csv") %>%
   rename(perim_start = ...1) %>%
   mutate(abs.curv = abs(Curvature)) %>%
   print()
@@ -128,7 +146,7 @@ ggplot(data = test.plot, #aes(x = perim_start, y = Curvature)
        ) +
   geom_point() +
   #xlab("perimeter position") #+
-  geom_vline(xintercept = 3507)
+  geom_vline(xintercept = 836)
   # geom_vline(xintercept = 2063)
 
 
